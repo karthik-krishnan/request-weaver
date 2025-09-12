d9911f88947b96b2665412e4d74d3b9e38135f2f
Karthik Krishnan <me@karthiks.in>
2025-09-11 20:46:51 -0400

    ensuring json rendering is on priority before defaulting to html rendering of message

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -2,6 +2,6 @@ const r3 = require("express").Router();
 const c3 = require("../controllers/dashboard.controller");
 r3.get("/dashboard/html", c3.html);
 r3.get("/dashboard/data", c3.data);
-r3.get("/messages/:messageId", c3.messageHtml);
 r3.get("/messages/:messageId.json", c3.messageJson);
+r3.get("/messages/:messageId", c3.messageHtml);
 module.exports = r3;
